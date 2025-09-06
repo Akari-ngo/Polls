6ec40a5a69e963b73bea91f639750407bfe18503
Akari-ngo <akari.ringo.625@gmail.com>
2025-09-06 16:02:41 +0900

    ビュー

--- main/views.py
+++ main/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+#関数ベースビュー
+#ビュー関数
+def index(repuest):
+    return HttpResponse("カメラが欲しいっ！")
+
+
