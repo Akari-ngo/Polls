cd8ad412eeb019cc44f32dcd0ff8481a8803aa6c
Akari-ngo <akari.ringo.625@gmail.com>
2025-09-13 18:29:29 +0900

    テンプレート作成

--- main/views.py
+++ main/views.py
@@ -4,6 +4,16 @@ from django.http import HttpResponse
 #関数ベースビュー
 #ビュー関数
 def index(repuest):
-    return HttpResponse("カメラが欲しいっ！")
-
-
+    question_list = [
+        "ミセスは好きですよね?",
+        "もちろんハロウィン！ゾンビはお好きで？",
+        "もちろんユニバ派ですよね？"
+    ]
+    context = {
+       "question_list": question_list,
+       "is_polled": True,
+       "polled_msg": "投票ありがとうございました🙇",
+       "not_polled_msg": "投票して下さい（圧）",
+       "user_name": "UDSAJIFSAUNK!I",
+    }
+    return render(repuest, "main/index.html", context)
