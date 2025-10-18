1e90be2fd7b2e080ebbe4202174cdb66094917a2
Akari-ngo <akari.ringo.625@gmail.com>
2025-10-18 16:29:24 +0900

    css

--- main/views.py
+++ main/views.py
@@ -1,19 +1,34 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 
+from .models import Question, Choice
 #関数ベースビュー
 #ビュー関数
-def index(repuest):
-    question_list = [
-        "ミセスは好きですよね?",
-        "もちろんハロウィン！ゾンビはお好きで？",
-        "もちろんユニバ派ですよね？"
-    ]
+def index(request):
+    all_question = Question.objects.all()
     context = {
-       "question_list": question_list,
-       "is_polled": True,
-       "polled_msg": "投票ありがとうございました🙇",
-       "not_polled_msg": "投票して下さい（圧）",
-       "user_name": "UDSAJIFSAUNK!I",
+        "all_question" : all_question,
     }
-    return render(repuest, "main/index.html", context)
+
+    return render(request, "main/index.html", context)
+
+def detail(request, question_id):
+    question = Question.objects.get(pk=question_id)
+    context = {
+        "question": question,
+    }
+    return render(request, "main/detail.html", context)
+
+def vote(request, question_id):
+    question = Question.objects.get(pk=question_id)
+    try:
+        selected_choice = question.choices.get(pk=request.POST['choice'])
+    except (KeyError,Choice.DoesNotExist):
+        return render(request, 'main/detail.html', {
+            'question':question,
+            'error_message':"You didn't delect a choice", 
+        })
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        return redirect("detail", question_id)
